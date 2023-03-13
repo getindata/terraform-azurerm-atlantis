cd8855c9fb0e4e19d2f0581091dc02487b094767
Mariusz Wojakowski <mariusz89016@gmail.com>
2023-03-13 11:42:53 +0100

    fix: Add `infracost` option and remove defaults to handle fallbacks properly (#12)
    * fix: Add `infracost` option and remove defaults to handle fallbacks properly
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: mariusz.wojakowski <mariusz.wojakowski@truecaller.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "atlantis_repo_config" {
   source  = "getindata/atlantis-repo-config/null"
-  version = "2.1.0"
+  version = "2.1.1"
 
   repos               = var.atlantis_repo_config_repos
   repos_common_config = var.atlantis_repo_config_repos_common_config
