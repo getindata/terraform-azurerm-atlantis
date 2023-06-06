b3f114e3d40e06f8d0c1a705f0b0a637cb2c0617
Mariusz Wojakowski <mariusz89016@gmail.com>
2023-06-06 14:00:56 +0200

    feat: Use `getindata/atlantis-repo-config/null` that handles `import` & `state rm` steps (#15)

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "atlantis_repo_config" {
   source  = "getindata/atlantis-repo-config/null"
-  version = "2.2.0"
+  version = "2.3.0"
 
   repos               = var.atlantis_repo_config_repos
   repos_common_config = var.atlantis_repo_config_repos_common_config
